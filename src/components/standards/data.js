a11995a54751cda7146e9ae9e2574afe982cf30f
片岡潤隆 <>
2022-02-14 01:41:10 +0900

    データ作成

---
+++ src/components/standards/data.js
@@ -0,0 +1,67 @@
+export const listData = [
+  { id: "01", text: "Sulfates SLS and SLES", number: " 1" },
+  {
+    id: "02",
+    text: "Paraffin Oil",
+    number: 2,
+  },
+  {
+    id: "03",
+    text: "Formaldehyde-releasing agents",
+    number: 3,
+  },
+  {
+    id: "04",
+    text: "Coal tar",
+    number: 4,
+  },
+  {
+    id: "05",
+    text: "Triclosan",
+    number: 5,
+  },
+  {
+    id: "06",
+    text: "Methylisothiazolinone",
+    number: 6,
+  },
+  {
+    id: "07",
+    text: "Phthalates、Dibutyl phthalate",
+    number: 7,
+  },
+  {
+    id: "08",
+    text: "Retinyl palmitate",
+    number: 8,
+  },
+  {
+    id: "09",
+    text: "Oxybenzone",
+    number: 9,
+  },
+  {
+    id: "10",
+    text: "Hydroquinone",
+    number: 10,
+  },
+  {
+    id: "11",
+    text: "Triclocarban",
+    number: 11,
+  },
+  {
+    id: "12",
+    text: "BHA and BHT※1",
+    number: 12,
+  },
+  {
+    id: "13",
+    text: "DEA-related ingredients",
+    number: 13,
+  },
+  { id: "14", text: "Resorcinol", number: 14 },
+  { id: "15", text: "Benzalkonium Chloride", number: 15 },
+  { id: "16", text: "Parabens", number: 16 },
+  { id: "17", text: "Microplastic beeds", number: 17 },
+];
---
+++ src/components/store/data.js
@@ -0,0 +1,38 @@
+export const storeData = [
+  {
+    name: "DOUGLAS MADRID",
+    address: "Paseo De Las Acacias, 23 28005 MADRID, MADRID",
+    map_link: "",
+  },
+  {
+    name: "DOUGLAS MADRID 2",
+    address: "Calle Arturo Soria, 325 28033 MADRID, MADRID",
+    map_link: "",
+  },
+  {
+    name: "DOUGLAS PAMPLONA",
+    address: "Calle Sancho El Mayor , 4 31002 PAMPLONA, NAVARRA",
+    map_link: "",
+  },
+  {
+    name: "DOUGLAS MADRID",
+    address: "Calle Juan De Garay, 6 48901 BARAKALDO, VIZCAYA",
+    map_link: "",
+  },
+  {
+    name: "DOUGLAS VIZCAYA",
+    address: "Avenida Murrieta, 2 48980 SANTURCE, VIZCAYA",
+    map_link: "",
+  },
+  {
+    name: "DOUGLAS POZUELO",
+    address:
+      "Centro comercial Zielo De Pozuelo, 28224 POZUELO DE ALARCÓN, MADRID",
+    map_link: "",
+  },
+  {
+    name: "DOUGLAS SAN SEBASTIAN",
+    address: "Calle Matia , 19 20008 SAN SEBASTIAN, GUIPÚZCOA",
+    map_link: "",
+  },
+];
