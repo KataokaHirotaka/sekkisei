6383b68b7519c0a3212e37fab497bda2fd92f6dd
片岡潤隆 <>
2022-02-15 12:17:04 +0900

    設定の変更、モジュールのインポート

--- next.config.js
+++ next.config.js
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  sassOptions: {
-    prependData: `
-    @use '@variables' as *;
-    `,
-  },
+  // sassOptions: {
+  //   prependData: `
+  //   @use '@variables' as *;
+  //   `,
+  // },
 };
 
 module.exports = nextConfig;
