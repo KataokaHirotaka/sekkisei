0a2a843100e4c5a4f4034b19b44d66a2dc8a15e6
片岡潤隆 <>
2022-02-15 12:17:23 +0900

    ingridientsデータの作成

---
+++ src/components/ingredients/data.js
@@ -0,0 +1,23 @@
+export const data = [
+  {
+    id: "peonyExtra",
+    name: "Peony Extra",
+    from: "from Niigata",
+    effect: "Supporting the skin's barrier membrane",
+    img: "/assets/img/peony_extra.png",
+  },
+  {
+    id: "noniJuice",
+    name: "Noni Juice",
+    from: "from Okinawa",
+    effect: "high oxidation",
+    img: "/assets/img/noni_juice.png",
+  },
+  {
+    id: "alpinaLeafExtra",
+    name: "Alpina Leaf Extra",
+    from: "from Okinawa",
+    effect: "high oxidation and Brightening effects",
+    img: "/assets/img/alpina_leaf_extra.png",
+  },
+];
