8966307935981a8703a4d42127c09d934d7e0cc3
片岡潤隆 <>
2022-02-15 12:13:11 +0900

    画像をImageタグで設定

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,13 +1,23 @@
+/* eslint-disable @next/next/no-img-element */
 /* eslint-disable jsx-a11y/alt-text */
 import Image from "next/image";
 import styles from "src/components/footer/index.module.scss";
+import { Link as Scroll } from "react-scroll";
 
 export const Footer = (): JSX.Element => {
   return (
     <footer className={styles.footer}>
-      <div className={styles.scroll_btn}>
-        <img src="/assets/img/arrow.svg" alt="" />
-      </div>
+      <Image
+        src="/assets/img/footer_main_sp@2x.png"
+        layout="fill"
+        objectFit="cover"
+      />
+      <Scroll to="top" smooth={true} duration={600}>
+        <div className={styles.scroll_btn}>
+          <img src="/assets/img/arrow.svg" alt="" />
+        </div>
+      </Scroll>
+
       <p>Copyright © KOSÉ Corporation. All rights reserved.</p>
     </footer>
   );
--- src/components/hero/index.tsx
+++ src/components/hero/index.tsx
@@ -4,21 +4,26 @@ import Image from "next/image";
 
 export const Hero = (): JSX.Element => {
   return (
-    <div className={styles.topHero_wrapper}>
+    <div className={styles.topHero_wrapper} id="top">
+      <Image
+        src="/assets/img/main_sp@2x.png"
+        layout="fill"
+        objectFit="cover"
+        objectPosition="center"
+        alt=""
+      />
       <div className={styles.topHero}>
         <Header />
       </div>
       <h1 className={styles.hero_title}>
-        {/* TODOImageタグに変える必要あり */}
-        <img src="/assets/img/hero_title@2x.png" alt="sekkisei" />
-        {/* <Image
-          src="/assets/img/hero_title@2x.png"
-          width={1000}
-          height={87}
-          objectFit="contain"
-          alt="sekkisei"
-          unoptimized
-        /> */}
+        <div>
+          <Image
+            src="/assets/img/hero_title@2x.png"
+            objectFit="contain"
+            layout="fill"
+            alt="sekkisei"
+          />
+        </div>
       </h1>
     </div>
   );
