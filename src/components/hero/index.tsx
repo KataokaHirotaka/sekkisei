38fa5975cde166ea2a1ea36dd456fe1fc720fd3c
片岡潤隆 <>
2022-02-20 21:40:42 +0900

    メイン画像の修正

--- src/components/hero/index.tsx
+++ src/components/hero/index.tsx
@@ -5,25 +5,35 @@ import Image from "next/image";
 export const Hero = (): JSX.Element => {
   return (
     <div className={styles.topHero_wrapper} id="top">
-      <Image
-        src="/assets/img/main_sp@2x.png"
-        layout="fill"
-        objectFit="cover"
-        objectPosition="center"
-        alt=""
-      />
+      <div className={`${styles.main_img_wrapper} ${styles.img_sp}`}>
+        <Image
+          src="/assets/img/main_sp@2x.png"
+          layout="fill"
+          objectFit="cover"
+          objectPosition="center"
+          alt=""
+        />
+      </div>
+      <div className={`${styles.main_img_wrapper} ${styles.img_pc}`}>
+        <Image
+          src="/assets/img/main_pc@2x.png"
+          layout="fill"
+          objectFit="cover"
+          objectPosition="center"
+          alt=""
+        />
+      </div>
+
       <div className={styles.topHero}>
         <Header />
       </div>
       <h1 className={styles.hero_title}>
-        <div>
-          <Image
-            src="/assets/img/hero_title@2x.png"
-            objectFit="contain"
-            layout="fill"
-            alt="sekkisei"
-          />
-        </div>
+        <Image
+          src="/assets/img/hero_title@2x.png"
+          objectFit="contain"
+          layout="fill"
+          alt="sekkisei"
+        />
       </h1>
     </div>
   );
