f2edf5a185f1c61d6e6296150a7a721a27aea235
片岡潤隆 <>
2022-02-20 21:45:39 +0900

    SnsLinkコンポーネントの修正

--- src/components/sns-link/index.tsx
+++ src/components/sns-link/index.tsx
@@ -1,13 +1,21 @@
 import Image from "next/image";
 import styles from "src/components/sns-link/index.module.scss";
 
-export const SnsLink = (): JSX.Element => {
+type Props = {
+  type: string | undefined;
+};
+
+export const SnsLink = (props: Props): JSX.Element => {
   return (
-    <a className={styles.sns_link}>
+    <a className={`${styles.sns_link} ${props.type ? styles.white : null}`}>
       Official SNS
       <div className={styles.link_img_wrapper}>
         <Image
-          src="/assets/img/instagram.svg"
+          src={
+            props.type
+              ? "/assets/img/instagram_white.svg"
+              : "/assets/img/instagram.svg"
+          }
           width={34}
           height={34}
           alt="sns"
