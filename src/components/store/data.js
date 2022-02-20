edf620f208e16f0b0c8c8be5d0d39332d2f96995
片岡潤隆 <>
2022-02-20 21:46:54 +0900

    Storeコンポーネントの修正

--- src/components/store/data.js
+++ src/components/store/data.js
@@ -1,38 +1,44 @@
 export const storeData = [
   {
     name: "DOUGLAS MADRID",
-    address: "Paseo De Las Acacias, 23 28005 MADRID, MADRID",
+    address: "Paseo De Las Acacias, 23",
+    address_second: "28005 MADRID, MADRID",
     map_link: "",
   },
   {
     name: "DOUGLAS MADRID 2",
-    address: "Calle Arturo Soria, 325 28033 MADRID, MADRID",
+    address: "Calle Arturo Soria, 325",
+    address_second: "28033 MADRID, MADRID",
     map_link: "",
   },
   {
     name: "DOUGLAS PAMPLONA",
-    address: "Calle Sancho El Mayor , 4 31002 PAMPLONA, NAVARRA",
+    address: "Calle Sancho El Mayor , 4",
+    address_second: "31002 PAMPLONA, NAVARRA",
     map_link: "",
   },
   {
     name: "DOUGLAS MADRID",
-    address: "Calle Juan De Garay, 6 48901 BARAKALDO, VIZCAYA",
+    address: "Calle Juan De Garay, 6",
+    address_second: "48901 BARAKALDO, VIZCAYA",
     map_link: "",
   },
   {
     name: "DOUGLAS VIZCAYA",
-    address: "Avenida Murrieta, 2 48980 SANTURCE, VIZCAYA",
+    address: "Avenida Murrieta, 2",
+    address_second: "48980 SANTURCE, VIZCAYA",
     map_link: "",
   },
   {
     name: "DOUGLAS POZUELO",
-    address:
-      "Centro comercial Zielo De Pozuelo, 28224 POZUELO DE ALARCÓN, MADRID",
+    address: "Centro comercial Zielo De Pozuelo,",
+    address_second: "28224 POZUELO DE ALARCÓN, MADRID",
     map_link: "",
   },
   {
     name: "DOUGLAS SAN SEBASTIAN",
-    address: "Calle Matia , 19 20008 SAN SEBASTIAN, GUIPÚZCOA",
+    address: "Calle Matia , 19",
+    address_second: "20008 SAN SEBASTIAN, GUIPÚZCOA",
     map_link: "",
   },
 ];
--- src/components/store/index.tsx
+++ src/components/store/index.tsx
@@ -1,27 +1,35 @@
 import styles from "src/components/store/index.module.scss";
 import { storeData } from "src/components/store/data";
-import Image from "next/image";
 import { SnsLink } from "../sns-link";
+import { ScrollBtn } from "../scrollBtn";
 
 export const Store = (): JSX.Element => {
   return (
     <section id="find_a_store" className={styles.store_section}>
-      <h2>Find A Store</h2>
-      <ul className={styles.store_data_list}>
-        {storeData.map((data) => {
-          return (
-            <li className={styles.store_data_content} key={data.name}>
-              <p className={styles.store_name}>{data.name}</p>
-              <p className={styles.store_address}>{data.address}</p>
-              <a className={styles.store_map_link} href={data.map_link}>
-                MAP
-              </a>
-            </li>
-          );
-        })}
-      </ul>
-      <div className={styles.sns_link_wrapper}>
-        <SnsLink />
+      <div className={styles.container}>
+        <h2 className={styles.store_section_ttl}>FIND A STORE</h2>
+        <ul className={styles.store_data_list}>
+          {storeData.map((data) => {
+            return (
+              <li className={styles.data_list_content} key={data.name}>
+                <p className={styles.store_name}>{data.name}</p>
+                <p className={styles.store_address}>{data.address}</p>
+                <p className={styles.store_address}>{data.address_second}</p>
+                <a className={styles.store_map_link} href={data.map_link}>
+                  MAP
+                </a>
+              </li>
+            );
+          })}
+        </ul>
+        <div className={styles.store_module_wrapper}>
+          <div className={styles.snsLink_wrapper}>
+            <SnsLink type="" />
+          </div>
+          <div className={styles.scrollBtn_wrapper}>
+            <ScrollBtn />
+          </div>
+        </div>
       </div>
     </section>
   );
