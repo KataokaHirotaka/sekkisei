f03dbba8a4d61cf8c50d866e5452958b62a9260a
片岡潤隆 <>
2022-03-04 00:15:42 +0900

    Cardコンポーネントの修正

--- src/components/card/index.tsx
+++ src/components/card/index.tsx
@@ -28,46 +28,52 @@ export const Card = (props: Props): JSX.Element => {
 
   return (
     <div className={styles.product_card}>
-      <Image
-        src={propsData.img}
-        width={350}
-        height={350}
-        alt={propsData.title}
-      />
-      <h2 className={styles.card_title}>{propsData.title}</h2>
-      <span className={styles.card_supplement}>{propsData.supplement}</span>
-      <p className={styles.card_price}>{propsData.price}</p>
-      <div className={styles.card_section}>
-        <h3>Product detail</h3>
-        <p>{propsData.detail}</p>
+      <div className={styles.img_wrapper}>
+        <Image
+          src={propsData.img}
+          layout="fill"
+          objectFit="contain"
+          alt={propsData.title}
+        />
       </div>
-      <div className={styles.card_section}>
-        <h3>Skin type</h3>
-        <p>{propsData.type}</p>
-      </div>
-      <div className={styles.card_section}>
-        <h3>Concerns</h3>
-        <p>{propsData.concerns}</p>
-      </div>
-      <div className={`${styles.card_section} ${isClick ? styles.open : null}`}>
-        <h3 className={`${styles.list_title}`} onClick={clickHandle}>
-          Key Ingredients<span></span>
-        </h3>
+      <div className={styles.txt_wrapper}>
+        <h2 className={styles.card_title}>{propsData.title}</h2>
+        <span className={styles.card_supplement}>{propsData.supplement}</span>
+        <p className={styles.card_price}>{propsData.price}</p>
+        <div className={styles.card_section}>
+          <h3>Product detail</h3>
+          <p>{propsData.detail}</p>
+        </div>
+        <div className={styles.card_section}>
+          <h3>Skin type</h3>
+          <p>{propsData.type}</p>
+        </div>
+        <div className={styles.card_section}>
+          <h3>Concerns</h3>
+          <p>{propsData.concerns}</p>
+        </div>
+        <div
+          className={`${styles.card_section} ${isClick ? styles.open : null}`}
+        >
+          <h3 className={`${styles.list_title}`} onClick={clickHandle}>
+            Key Ingredients<span></span>
+          </h3>
 
-        <ul className={styles.card_section_list}>
-          <li>
-            <h3>Horsetail (Equisetum Arvense) Exract</h3>
-            <p>{propsData.ingredients[0].horsetail}</p>
-          </li>
-          <li>
-            <h3>Job&apos;s Tears Seed Extract</h3>
-            <p>{propsData.ingredients[0].tears}</p>
-          </li>
-          <li>
-            <h3>ITOWA</h3>
-            <p>{propsData.ingredients[0].itowa}</p>
-          </li>
-        </ul>
+          <ul className={styles.card_section_list}>
+            <li>
+              <h3>Horsetail (Equisetum Arvense) Exract</h3>
+              <p>{propsData.ingredients[0].horsetail}</p>
+            </li>
+            <li>
+              <h3>Job&apos;s Tears Seed Extract</h3>
+              <p>{propsData.ingredients[0].tears}</p>
+            </li>
+            <li>
+              <h3>ITOWA</h3>
+              <p>{propsData.ingredients[0].itowa}</p>
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
   );
